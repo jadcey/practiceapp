cc0e30de70d5955ce81a571e1ed1a1ed3824f2a4
Jade Gabuya <taekagay@gmail.com>
2024-04-14 01:48:19 +0800

    ambot

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import {useNavigate} from 'react-router-dom';
+import { DiCodeigniter } from "react-icons/di"
 const Header = ()=>{
     const navigate = useNavigate();
     const toHomeVape = () => {
@@ -12,6 +13,11 @@ const Header = ()=>{
     }
     return(
         <div className = "header-body">
+            <div className = "header-icon">
+                <DiCodeigniter/>
+                <div className="header-text">Name</div>
+                <div className="header-text">Email</div>
+            </div>
             <div className = "header-text" onClick={toHome}>Home</div>
             <div className = "header-text" onClick={toHomeVape}>VapeN'Hell</div>
             <div className = "header-text" onClick={toHomeGoodDudes}>Good Dudes</div>
